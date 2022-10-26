1f878894d36ee93f920cc68d0c65138753863926
밤밤 <112460285+marrron@users.noreply.github.com>
2022-10-26 17:57:05 +0900

    Update 배열의 평균값.js

--- 프로그래머스/lv0/120817. 배열의 평균값/배열의 평균값.js
+++ 프로그래머스/lv0/120817. 배열의 평균값/배열의 평균값.js
@@ -1,3 +1,11 @@
 function solution(numbers) {
   return numbers.reduce((a, c) => a + c, 0) / numbers.length;
 }
+
+function solution(numbers) {
+  let answer = 0;
+    for(i=0; i<numbers.length; i++){
+        answer += numbers[i] / numbers.length
+    }
+    return answer
+}
