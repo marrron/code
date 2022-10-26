ccb7e428aa100115d5610b50c5c4997d478b2a0e
밤밤 <112460285+marrron@users.noreply.github.com>
2022-10-26 17:18:26 +0900

    Update 암호 해독.js

--- 프로그래머스/lv0/120892. 암호 해독/암호 해독.js
+++ 프로그래머스/lv0/120892. 암호 해독/암호 해독.js
@@ -12,3 +12,9 @@ function solution(cipher, code) {
 
     return answer;
 }
+
+// function solution(cipher, code) {
+//     let answer = [];
+//     answer = cipher.split('').filter((item,index)=>((index+1)%code ===0)).join("")
+//     return answer;
+// }
