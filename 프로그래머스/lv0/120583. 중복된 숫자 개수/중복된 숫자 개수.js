7e89fbde908a8365d9f0c0892203785fe539819a
밤밤 <112460285+marrron@users.noreply.github.com>
2022-10-26 17:47:44 +0900

    Update 중복된 숫자 개수.js

--- 프로그래머스/lv0/120583. 중복된 숫자 개수/중복된 숫자 개수.js
+++ 프로그래머스/lv0/120583. 중복된 숫자 개수/중복된 숫자 개수.js
@@ -1,3 +1,13 @@
 function solution(array, n) {
   return array.filter(a => a === n).length;
 }
+
+function solution(array, n) {
+    var answer = 0;
+    for (var i = 0; i < array.length; i++){
+        if(array[i] == n){
+            answer++
+        }
+    }
+return answer;
+}
