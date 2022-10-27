1efe6dd24b575fe97a679afa5b01da80cc2bf64f
밤밤 <112460285+marrron@users.noreply.github.com>
2022-10-28 00:05:53 +0900

    Update 머쓱이보다 키 큰 사람.js

--- 프로그래머스/lv0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
+++ 프로그래머스/lv0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
@@ -1,3 +1,13 @@
 function solution(array, height) {
   return array.filter(a => a > height).length;
 }
+
+function solution(array, height) {
+    var answer = 0;
+    for (let i=0; i<array.length; i++){
+        if(array[i] > height){
+            answer++
+        }
+    }
+    return answer
+}
