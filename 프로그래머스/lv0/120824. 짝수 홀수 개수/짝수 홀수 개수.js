2c57ec9807d173764a89770467f0f71046586914
밤밤 <112460285+marrron@users.noreply.github.com>
2022-11-04 01:01:44 +0900

    Update 짝수 홀수 개수.js

--- 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -2,3 +2,14 @@ function solution(num_list) {
   const evenLength = num_list.filter(n => n % 2 === 0).length;
   return [evenLength, num_list.length - evenLength];
 }
+
+function solution(num_list) {
+    var answer = [0, 0];
+
+    for(let i = 0 ; i < num_list.length; i++){
+        if(num_list[i] % 2 == 0) answer[0]++;
+        else answer[1]++;
+    }
+
+    return answer;
+}
